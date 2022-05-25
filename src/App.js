11ce70a8c1cadd613658fdd64ef049b76ea59672
knguyen111601 <knguyen111601@gmail.com>
2022-05-25 12:13:32 -0700

    comments to describe what is happening with each button press

--- src/App.js
+++ src/App.js
@@ -19,11 +19,27 @@ function App() {
 
   return (
     <div>
-      <h1>Counter App</h1>
-      <h2>{counter}</h2>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-      <button onClick={addBy}>Add by 10</button>
+      <h1 style={{textAlign:"center"}}>Counter App</h1>
+      <h2 style={{textAlign:"center"}}>{counter}</h2>
+
+      <div style={{display:"flex", flexDirection:"column", maxWidth:"80%", margin:"auto"}}>
+        <div style={{display:"flex", justifyContent:"space-around"}}>
+          <button onClick={increment} style={{width: "40%"}}>Increment</button>
+          <p style={{width: "40%"}}>Dispatches the type of "increment" which the reducer function receives and returns the state after incrementing</p>
+        </div>
+
+        <div style={{display:"flex", justifyContent:"space-around"}}>
+          <button onClick={decrement} style={{width: "40%"}}>Decrement</button>
+          <p style={{width: "40%"}}>Dispatches the type of "decrement" which the reducer function receives and returns the state after decrementing</p>
+        </div>
+
+        <div style={{display:"flex", justifyContent:"space-around"}}>
+          <button onClick={addBy} style={{width: "40%"}}>Add by 10</button>
+          <p style={{width: "40%"}}>Dispatches the type of "addby" and payload of 10. The reducer function receives the type to discover which action to complete and then utilizes the data from the payload to commit the action. It then returns the state</p>
+        </div>
+      </div>
+
+
     </div>
   );
 }
